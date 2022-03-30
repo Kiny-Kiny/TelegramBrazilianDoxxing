d47d7a7dd4d36b2d63b88afc25a315924ac39831
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2022-03-30 14:59:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from json import loads, dumps
 from os import system, path
 from sys import argv
 from telethon import TelegramClient, connection, sync, events
+from telethon.tl.functions.channels import JoinChannelRequest
 from time import sleep
 
 s = Session();
@@ -116,6 +117,50 @@ def cpf_prodata(message):
     ###################
     return resultado
 
+def placa_prodata(message):
+    dados = {}
+    ####################
+    dados['dadosGerais'] = {
+        'placaAtual': message.split('PLACA ATUAL: ')[1].split('\n')[0],
+        'placaNova': message.split('PLACA NOVA: ')[1].split('\n')[0],
+        'placaAntiga': message.split('PLACA ANTIGA: ')[1].split('\n')[0],
+        'restricoes': message.split('RESTRIÇÕES: ')[1].split('\n')[0],
+        'renavam': message.split('RENAVAM: ')[1].split('\n')[0],
+        'chassi': message.split('CHASSI: ')[1].split('\n')[0]
+    }
+    dados['marcaModelo'] = {
+        'modelo': message.split('MODELO: ')[1].split('\n')[0],
+        'marca': message.split('MARCA: ')[1].split('\n')[0],
+        'segmento': message.split('SEGMENTO: ')[1].split('\n')[0],
+        'subSegmento': message.split('SUB-SEGMENTO: ')[1].split('\n')[0],
+        'grupo': message.split('GRUPO: ')[1].split('\n')[0],
+    }
+    dados['composicao'] = {
+        'combustivel': message.split('COMBUSTÍVEL: ')[1].split('\n')[0],
+        'potencia': message.split('POTÊNCIA: ')[1].split('\n')[0],
+        'potenciaDeCarga': message.split('POTÊNCIA DE CARGA: ')[1].split('\n')[0],
+        'motor': message.split('MOTOR: ')[1].split('\n')[0],
+        'eixos': message.split('EIXOS: ')[1].split('\n')[0]
+    }
+    dados['fabricacao'] = {
+        'anoModelo': message.split('ANO MODELO: ')[1].split('\n')[0],
+        'anoFabricacao': message.split('ANO FABRICAÇÃO: ')[1].split('\n')[0],
+        'nacionalidadeFab': message.split('NACIONALIDAE FAB.: ')[1].split('\n')[0],
+        'linhaFab': message.split('LINHA FAB.: ')[1].split('\n')[0],
+        'motorFab': message.split('MOTOR FAB.: ')[1].split('\n')[0],
+        'tipoCarroceria': message.split('TIPO CARROCERIA: ')[1].split('\n')[0]
+    }
+    dados['localizacao'] = {
+        'estado': message.split('ESTADO (UF): ')[1].split('\n')[0],
+        'municipio': message.split('MUNICIPIO: ')[1].split('\n')[0],
+        'municipioEmplacado': message.split('MUNICIPIO EMPLACADO: ')[1].split('\n')[0]
+    }
+    dados['dadosCondutor'] = {
+        'tipoDocumento': message.split('TIPO DOCUMENTO: ')[1].split('\n')[0],
+        'documento': message.split('DOCUMENTO: ')[2].split('\n')[0],
+        'nomeCompleto': message.split('NOME COMPLETO: ')[1].split('\n')[0]
+    }
+    return dados
 ############################################
 
 def login():
@@ -186,13 +231,19 @@ def login():
 
 ############################################
 
-def main(args, link = "https://t.me/PuxadasGratis24hrs"):
+def main(args, user = "@PuxadasGratis24hrs"):
     if len(args) < 2:
         return {'status': 402, 'message': 'Alguns parâmetros estão faltando!'}
-    elif args[0] not in ['/cpf', '/tel']:
+    elif args[0] not in ['/cpf', '/tel', '/placa']:
         return {"status": 403, "message": "Comando Inválido!"}
         
     message = '';
+    
+    errorMessage = {
+        '/tel': 'TELEFONE NÃO ENCONTRADO',
+        '/cpf': 'CPF NÃO ENCONTRADO',
+        '/placa': 'PLACA NÃO ENCONTRADA'
+    }
     
     if not path.exists('dados.json'):
         retorno = login()
@@ -226,41 +277,52 @@ def main(args, link = "https://t.me/PuxadasGratis24hrs"):
     for i in args: message+= i+' ';
     
     try:
-            message = message[:-1];entity = client.get_entity(link);
+            try:
+                client(JoinChannelRequest(user))
+            except Exception:
+                pass
+                
+            message = message[:-1];entity = client.get_entity(user);
             
             try:
                 while True:
                     try:
-                        client.send_message(entity = entity, message = message.replace('/cpf', '/cpf3').replace('/tel', '/telefone1'))
+                        client.send_message(entity = entity, message = message.replace('/cpf', '/cpf3').replace('/tel', '/telefone1').replace('/placa', '/placa2'))
                         break
                     except:
                         pass
                 
                 while True:
-                    for messages in client.get_messages(entity):
+                    for messages in client.get_messages(entity, limit = 3):
                         id = messages.from_id.user_id
                         msg = messages.message
-                    sleep(1)
+                    sleep(0.5)
                     if id == 1747207086:
                         func = {
                         '/cpf': cpf_prodata,
-                        '/tel': tel_prodata
+                        '/tel': tel_prodata,
+                        '/placa': placa_prodata
                         }[args[0]]
-                        
-                        msg = func(msg)
                         break
                             
-                return {"status": 200, "message": msg}
-            except Exception as e:
-                return {"status": 400, "message": str(e)}
+                dados = {"status": 200, "message": func(msg)}
+            except Exception:
+                dados = {"status": 400, "message": errorMessage[args[0]]}
     except:
-        return  {"status": 500, "message": "ERRO NO SERVIDOR"}
+        dados =  {"status": 500, "message": "ERRO NO SERVIDOR"}
+        
+    try:
+        client.delete_dialog(entity)
+    except Exception as e:
+        print({'error': str(e)})
+        
+    return dados
 
 ############################################
      
 def start():
     with open('consulta.json', 'w+') as v:
         retorno = main(argv[1:])
-        return v.write(str(retorno))
+        return v.write(dumps(retorno))
         
-if __name__ == '__main__': start()
+if __name__ == '__main__': start() 
