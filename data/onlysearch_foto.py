d9d68a96d15597c0ef55a3d62950fc0f0d6a867b
Bruno Parziovanni <70079144+Kiny-Kiny@users.noreply.github.com>
2022-04-26 18:18:14 -0300

    Update onlysearch_foto.py

--- data/onlysearch_foto.py
+++ data/onlysearch_foto.py
@@ -2,7 +2,7 @@ from base64 import b64encode
 def consulta(message):
     dados = {}
     with open('foto.jpg', 'rb') as f:
-      foto = b64.encode(f.read())
+      foto = b64encode(f.read())
     ################
     dados['dadosPessoais'] = {
         'nome': message.split('NOME: ')[1].split('\n')[0],
@@ -10,6 +10,6 @@ def consulta(message):
         'pai': message.split('PAI: ')[1].split('\n')[0],
         'mae': message.split('MÃE: ')[1].split('\n')[0]
      }
-    dados['foto'] = str(foto)
+    dados['foto'] = foto.decode('utf-8')
     ################
     return dados
